7b2f115e13428ecc046352b056a82b3db115779e
profferlhastlyfaner07@gmail.com <profferlhastlyfaner07@gmail.com>
2023-04-26 18:02:14 -0700

    deleted

--- session.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-session_start();
-if ($_SESSION['status'] == "invalid" OR empty($_SESSION['status'])){
-    $_SESSION['status']= "invalid";
-    unset($_SESSION['email']);
-    header('Location: index.php');
-}
-?>
